4d13a5bc85d82036f9ee4caf2f14480b4a7c291c
Manaken <auriolbenoit0@gmail.com>
2025-10-26 09:33:36 +0100

    first commit

---
+++ plannifVue/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Prof from './assets/profs/Prof'
+
+function App() {
+
+  return (
+    <>
+      <Prof/>
+    </>
+  )
+}
+
+export default App
---
+++ plannifVue/src/assets/profs/Prof.tsx
@@ -0,0 +1,14 @@
+function Prof() {
+    return (    
+        <>
+            <div>Nom</div> <input type="text" name="prof_nom"/>
+            <br/>
+            <div>Prénom</div> <input type="text" name="prof_prenom"/>
+            <br/>
+            <div>Nombre d'heures</div><input type="text" name="prog_nb_heures"/>
+        </>
+    );
+}
+
+
+export default Prof;
